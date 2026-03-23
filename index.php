f7aaf6e32c745e51c03d3626712d9015cbe3f057
Adam Abundis <adamjabundis@gmail.com>
2026-03-23 13:06:34 -0700

    chore: add required fallback template (index.php)

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+defined('ABSPATH') || exit;
+
+// Silence is golden.
+// WordPress requires this file to exist as a fallback template.
+// front-page.php handles the homepage.
+// Custom page templates handle all inner pages.
+// If you are seeing this rendered in the browser, a template is missing.
