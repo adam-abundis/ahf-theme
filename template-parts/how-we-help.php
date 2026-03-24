2e6358d06afed776aa437a5d7d50ac28b56efe22
Adam Abundis <adamjabundis@gmail.com>
2026-03-24 10:56:38 -0700

    feat: add how we help section template (template-parts/how-we-help.php)

---
+++ template-parts/how-we-help.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+defined('ABSPATH') || exit;
+?>
+
+<section class="how-we-help" aria-labelledby="how-we-help-heading">
+    <div class="container">
+        <h2 id="how-we-help-heading" class="how-we-help__title">How We Help</h2>
+        <p class="how-we-help__intro">
+            Each year AHF offers support to pets and people in times of need. We provide financial assistance to individuals as well as worthy causes hosted by other organizations.
+        </p>
+        <div class="how-we-help__grid">
+            <article class="card">
+                <h3 class="card__title">Angel Fund</h3>
+                <p class="card__body">Right here in our neighborhoods, animals and animal lovers confront obstacles that threaten their ability to provide for basic needs.</p>
+                <a href="/how-we-help/angel-fund/" class="card__link">Learn about Angel Fund</a>
+            </article>
+            <article class="card">
+                <h3 class="card__title">Wildlife Assistance</h3>
+                <p class="card__body">Wildlife species struggle to adapt to environments that may no longer support their needs.</p>
+                <a href="/how-we-help/wildlife-assistance/" class="card__link">Learn about wildlife</a>
+            </article>
+            <article class="card">
+                <h3 class="card__title">Education</h3>
+                <p class="card__body">Pet lovers know the benefits of animals, but many struggle to provide basic care for their pets.</p>
+                <a href="/how-we-help/education/" class="card__link">Learn about education</a>
+            </article>
+            <article class="card">
+                <h3 class="card__title">Join Forces</h3>
+                <p class="card__body">By joining forces we can consolidate efforts to improve the lives of animals and those who care about them.</p>
+                <a href="/donate/" class="card__link">Donate today</a>
+            </article>
+        </div>
+    </div>
+</section>
