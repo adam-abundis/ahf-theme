dc29c3409ed633e46de17146854c7db01f0b3025
Adam Abundis <adamjabundis@gmail.com>
2026-03-24 11:07:02 -0700

    feat: add social proof section tempalte part (template-parts/social-proof.php)

---
+++ template-parts/social-proof.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+defined('ABSPATH') || exit;
+?>
+
+<section class="social-proof" aria-labelledby="social-proof-heading">
+    <div class="container">
+        <h2 id="social-proof-heading" class="social-proof__title">Latest News</h2>
+        <ul class="social-proof__list">
+            <li class="social-proof__item">
+                <a href="/news/item.html/n/20642" class="social-proof__link">Give Them Time</a>
+            </li>
+            <li class="social-proof__item">
+                <a href="/news/item.html/n/20641" class="social-proof__link">How to Do CPR On a Dog</a>
+            </li>
+            <li class="social-proof__item">
+                <a href="/news/item.html/n/20639" class="social-proof__link">Angel Fund Helps Puppy Stricken by Parvovirus</a>
+            </li>
+            <li class="social-proof__item">
+                <a href="/news/item.html/n/20633" class="social-proof__link">UF Small Animal Hospital makes history with first open-heart surgery procedure</a>
+            </li>
+        </ul>
+        <a href="/news/" class="social-proof__more">See All News</a>
+    </div>
+</section>
