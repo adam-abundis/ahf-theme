e9a3310e968579decc1c16380e0799561141d58a
Adam Abundis <adamjabundis@gmail.com>
2026-03-24 10:47:34 -0700

    feat: add empathy section template part (empathy.php)

---
+++ template-parts/empathy.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+defined('ABSPATH') || exit;
+?>
+
+<section class="empathy" aria-labelledby="empathy-heading">
+    <div class="container">
+        <h2 id="empathy-heading" class="empathy__title">
+            Vet bills don't wait. Neither do we.
+        </h2>
+        <p class="empathy__body">
+             We know what it feels like to love an animal and not know if you can afford to help them. That is exactly why AHF exists.
+        </p>
+        <ul class="empathy__list">
+            <li class="empathy__item">Encourage the human-animal bond</li>
+            <li class="empathy__item">Promote public education about animals</li>
+            <li class="empathy__item">Forward the study and advancement of animal health</li>
+            <li class="empathy__item">Aid domestic animals and their owners</li>
+            <li class="empathy__item">Support projects aiding wildlife</li>
+        </ul>
+    </div>
+</section>
